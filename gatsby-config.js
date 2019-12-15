59c082d293ed83e86bff217f208bed73df1e6b89
AntonioKristicevic <antonio.kristicevic@typeqast.com>
2019-12-15 15:47:46 +0100

    Add typography js

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,8 +61,11 @@ module.exports = {
       resolve: `gatsby-plugin-styled-components`,
       options: {},
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utilities/typography`,
+      },
+    },
   ],
 }
---
+++ src/utilities/typography.js
@@ -0,0 +1,28 @@
+import Typography from "typography"
+const typography = new Typography({
+  baseFontSize: "20px",
+  baseLineHeight: 1.5,
+  scaleRatio:2.25,
+  headerFontFamily: [
+    "Montserrat",
+    "Avenir Next",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Montserrat","Georgia", "serif"],
+  includeNormalize:true,
+  googleFonts: [
+    {
+      name: 'Montserrat',
+      styles: [
+        '900',
+        '700',
+        '400',
+      ],
+    },
+]
+})
+export default typography

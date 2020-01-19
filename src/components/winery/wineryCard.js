1b0d3f0f61b1c9b849edd560681514bfa15c722d
AntonioKristicevic <antonio.kristicevic@typeqast.com>
2020-01-19 18:37:06 +0100

    Restyle wineries grid

--- src/components/winery/wineryCard.js
+++ src/components/winery/wineryCard.js
@@ -1,45 +1,48 @@
-import React from "react";
+import React from "react"
 import styled from "styled-components"
 import Img from "gatsby-image"
 
 import { hoverAnimationStyle } from "../common/container"
-import Divider from "../common/divider"
 import { EllipsisParagraph } from "../common/paragraphs"
 
-export default ({ image, name, description }) => (
-    <WineryCardContainer>
-        <div>
-            <WineryName>{name}</WineryName>
-            <Divider margin="0.75rem 25px" />
-            <WineryDescription lineNumber={7}>{description}</WineryDescription>
-        </div>
-        <WineryThumbnail fixed={image} />
-    </WineryCardContainer>
-)
-
 const WineryCardContainer = styled.div`
-    ${hoverAnimationStyle}    
-    display: flex;    
-    vertical-align: middle;
-    box-sizing:content-box;
+  ${hoverAnimationStyle}
+  display: flex;
+  vertical-align: middle;
+  box-sizing: content-box;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 6px;
 
-    border: 1px solid rgba(0,0,0,0.3);
-
-    &:hover{
-        cursor: pointer;
-    }
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0 0 3px #5b0b0b;
+  }
 `
 
 const WineryName = styled.h3`
-    margin: 25px 0 0 25px;
+  margin: 2.5% 0 2.5% 2.5%;
 `
 
 const WineryDescription = styled(EllipsisParagraph)`
-    margin: 0 0 25px 25px;    
-    font-size:medium;
+  margin: 0 0 2.5% 2.5%;
+  font-size: 100%;
 `
 
 const WineryThumbnail = styled(Img)`
-    margin: 25px;
-    min-width:250px;
+  margin: 2.5%;
+  flex-grow: 1;
 `
+
+const ContentContainer = styled.div`
+  max-width: 60%;
+`
+
+export default ({ image, name, description }) => (
+  <WineryCardContainer>
+    <ContentContainer>
+      <WineryName>{name}</WineryName>
+      <WineryDescription lineNumber={7}>{description}</WineryDescription>
+    </ContentContainer>
+    <WineryThumbnail fixed={image} />
+  </WineryCardContainer>
+)

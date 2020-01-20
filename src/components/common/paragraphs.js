f13eadc53094a34459d2532f26d0bc113f225a48
AntonioKristicevic <antonio.kristicevic@typeqast.com>
2020-01-20 18:09:27 +0100

    Add wine hero image

--- src/components/common/paragraphs.js
+++ src/components/common/paragraphs.js
@@ -1,17 +1,24 @@
 import styled from "styled-components"
 
+export const StyledParagraph = styled.p`
+  text-align: ${props => (props.centered ? "center" : "left")};
+  font-size: ${props => (props.fontSize ? props.fontSize : null)};
+  font-weight: ${props => (props.fontWeight ? props.fontWeight : "normal")};
+  margin: ${props => (props.margin ? props.margin : 0)};
+`
+
 export const CenteredParagraph = styled.p`
-    text-align:center;
+  text-align: center;
 `
 
 export const SubscriptParagraph = styled.p`
-    color:rgba(0,0,0,0.5);
-    font-size:small;
+  color: rgba(0, 0, 0, 0.5);
+  font-size: small;
 `
 
 export const EllipsisParagraph = styled.p`
-    overflow: hidden;
-    display: -webkit-box;
-    -webkit-line-clamp: ${props => props.lineNumber ? props.lineNumber : null};
-    -webkit-box-orient: vertical;  
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: ${props => (props.lineNumber ? props.lineNumber : null)};
+  -webkit-box-orient: vertical;
 `
